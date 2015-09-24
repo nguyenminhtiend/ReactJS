edb19539607a824ad81afb5f32f787a7d2729f59
nguyenminhtiend <nguyenminhtiend@gmail.com>
2015-09-24 14:21:53 +0700

    Init

---
+++ React/React/js/react-component/employee.jsx
@@ -0,0 +1,113 @@
+﻿var employees = [{name: 'Messi', department: 'IT', phone: '12345678'}];
+
+var Cell = React.createClass({   
+    render: function () {
+        return <td>{this.props.data}</td>
+    }
+});
+
+var CellHeader = React.createClass({   
+    render: function () {
+        return <th>{this.props.data}</th>
+    }
+});
+
+var Row = React.createClass({
+    render: function () {
+        return (<tr>
+            <Cell data={this.props.data.name} />
+            <Cell data={this.props.data.department} />
+            <Cell data={this.props.data.phone} />
+        </tr>);
+    }
+});
+
+var HeaderTable = React.createClass({
+    render: function () {
+        return (<thead><tr>
+            <CellHeader data={this.props.data.name} />
+            <CellHeader data={this.props.data.department} />
+            <CellHeader data={this.props.data.phone} />
+        </tr></thead>);
+}
+});
+
+var Grid = React.createClass({
+    render: function () {
+        var dataHeader = {
+            name: 'Name',
+            department: 'Department',
+            phone: 'Phone'
+        };
+
+        var header = (<HeaderTable data={dataHeader} />);
+
+        var body =  (<tbody>{
+            this.props.data.map(function (rowData, index) {
+                return <Row key={index} data={rowData} />;
+            })}</tbody>);
+
+        return (<div>
+                    <table className="table table-striped table-hover table-bordered">
+                    {header}{body}
+                    </table>
+                </div>
+            );
+    }
+});
+
+var EmloyeeForm = React.createClass({
+    render: function () {
+        return (<div className="panel panel-primary">
+                    <div className="panel-heading">Employee Management</div>
+                    <div className="panel-body">
+                        <form className="form-horizontal">
+                            {this.renderTextInput('name', 'Name:')}
+                            {this.renderTextInput('department', 'Department:')}
+                            {this.renderTextInput('phone', 'Phone:')}
+                        </form>
+                    </div>
+                </div>
+        )
+    },
+    renderField: function(id, label, field) {
+        return <div className="form-group col-xs-6">
+                <label htmlFor="{id}" className="col-sm-4 control-label">{label}</label>
+                <div className="col-sm-8">
+                    {field}
+                </div>
+            </div>
+    },
+    renderTextInput: function(id, label) {
+        return this.renderField(id, label, <input type="text" className="form-control" id={id} ref={id}/>)
+    },
+    getFormData: function() {
+        return {
+            name: this.refs.name.getDOMNode().value,
+            department: this.refs.department.getDOMNode().value,
+            phone: this.refs.phone.getDOMNode().value
+        };
+    }
+});
+
+var App = React.createClass({   
+    getInitialState: function() {
+        return {
+            gridData: employees
+        }
+    },
+    render: function () {
+        return <div>
+                <EmloyeeForm ref="employeeForm" />
+                <div className="pull-right">
+                    <button type="button" className="btn btn-primary btn-block" onClick={this.handleSubmit}>Submit</button>
+                </div>
+                <Grid data={this.state.gridData} />
+            </div>
+    },
+    handleSubmit: function() {
+        this.setState({ gridData: this.state.gridData.concat([this.refs.employeeForm.getFormData()]) })
+    }
+});
+
+React.render(<App  />, document.getElementById('employeeList'));
---
+++ React/React/js/react-component/todo-app.jsx
@@ -0,0 +1,40 @@
+﻿var TodoList = React.createClass({
+    render: function() {
+        var createItem = function(itemText) {
+            return <li>{itemText}</li>;
+        };
+    return <ul>{this.props.items.map(createItem)}</ul>;
+  }
+});
+
+var TodoApp = React.createClass({
+    getInitialState: function() {
+        return {items: [], text: ''};
+    },
+
+    onChange: function(e) {
+        this.setState({text: e.target.value});
+    },
+
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var nextItems = this.state.items.concat([this.state.text]);
+        var nextText = '';
+        this.setState({items: nextItems, text: nextText});
+    },
+
+    render: function() {
+        return (
+        <div>
+            <h3>TODO</h3>
+            <form onSubmit={this.handleSubmit}>
+                <input onChange={this.onChange} value={this.state.text} />
+                <button>{'Add #' + (this.state.items.length + 1)}</button>
+            </form>
+            <TodoList items={this.state.items} />
+        </div>
+        );
+    }
+});
+
+React.render(<TodoApp />, document.getElementById('todoExample'));
